37329fcfdbc83351ffb0bb15dffcb9b0ded66a6c
Samadani <Samadani@DESKTOP-FK0JHMS>
2024-05-07 15:27:38 +0330

    Add project files.

---
+++ DynamicDashboard/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using DynamicDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamicDashboard.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
